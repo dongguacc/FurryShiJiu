53dce3b7ebec3a0ef5e93533b66527074d4afe28
ShiJiu <153283866+furry19sj@users.noreply.github.com>
2024-09-30 21:24:29 +0800

    Add files via upload

--- js/w.js
+++ js/w.js
@@ -55,3 +55,17 @@ function topFunction() {
 
     requestAnimationFrame(animation);
 }
+
+(function() {
+    // 获取用户代理字符串
+    var userAgent = navigator.userAgent;
+
+    // 正则表达式检测手机设备
+    var isMobile = /iPhone|iPad|iPod|Android|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+
+    if (isMobile) {
+        // 如果是手机设备，重定向
+        window.location.href = "../p/p.html";
+    }
+    // 如果是电脑，则继续访问，不做任何操作
+})();
